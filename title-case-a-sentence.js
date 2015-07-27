a02025b685645f7152c1f72470ff9c9e209cbf65
Jay Wilson Jr <info@jaywilsonjr.com>
2015-07-27 15:10:38 -0400

    added comment block

--- title-case-a-sentence.js
+++ title-case-a-sentence.js
@@ -1,3 +1,10 @@
+// FreeCodeCamp.com Basic Bonfire: Title Case a Sentence
+
+// Instructions: Return the provided string with the first letter of each word capitalized. 
+// For the purpose of this exercise, you should also capitalize connecting words like 'the' and 'of'.
+
+// Solution created by: Jay Wilson Jr. 7/27/15 
+
 function titleCase(str) {
   var split = str.toLowerCase().split(" ");
   var newArr = [];
